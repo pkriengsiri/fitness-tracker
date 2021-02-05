9251ca8c82a23dbf0556af441e1256af65b72fca
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-02-05 18:58:08 -0500

    Added comments to server.js

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
+// Dependencies
 const express = require("express");
 const mongoose = require("mongoose");
 
+// Express server port
 const PORT = process.env.PORT || 3000;
 
+// Express instance
 const app = express();
 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
+// MongoDB connection
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -17,11 +21,11 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useCreateIndex: true,
 });
 
-// routes
+// Use routes
 app.use(require("./routes/api.js"));
 app.use(require("./routes/views.js"));
 
-
+// Listen to ports
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
